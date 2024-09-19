55a2cf37bd006f4e53f1467bd379ceddf0891fbf
Hamish Hunter <u7498619@anu.edu.au>
2024-09-19 14:37:41 +1000

    Initial Package commit

--- src/LinearODESolver.jl
+++ src/LinearODESolver.jl
@@ -1,5 +1,26 @@
 module LinearODESolver
+using Plots
+using DifferentialEquations
+using Test
 
-# Write your package code here.
+export define_system, solve_system, plot_solution
+
+function define_system(A::Matrix, u0::Vector, tspan::Tuple)
+    function LinearODE!(du, u, p, t)
+        du .= A * u
+    end
+
+    return ODEProblem(LinearODE!, u0, tspan)
+end
+
+function solve_system(prob::ODEProblem)
+    return solve(prob)
+end
+
+function plot_solution(sol::ODESolution)
+    plot(sol, xlabel = "Time", ylabel = "y", title = "Solution of ODE with A")
+end
+
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,21 @@ using LinearODESolver
 using Test
 
 @testset "LinearODESolver.jl" begin
-    # Write your tests here.
+
+    A = [0.0 1.0; -1.0 0.0]  # System of a simple harmonic oscillator
+    u0 = [1.0, 0.0]          # Initial conditions
+    tspan = (0.0, 10.0)      # Time span
+
+    prob = define_system(A, u0, tspan)
+    sol = solve_system(prob)
+
+    println(typeof(sol))
+    
+    @test typeof(sol) == ODESolution
+    @test isapprox(sol[1,13], cos(10), atol=1e-2)  # Verify result at t = 10.0
+    @test isapprox(sol[2,13], -sin(10), atol=1e-2) # Check solution of oscillator
+
+    # 3. Test plot_solution (just test that it runs without error)
+    @test plot_solution(sol) isa Plots.Plot
+
 end
